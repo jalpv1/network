c3a2c52acc18cf133786257e29613e389c1c6641
Julia <jalpaeva@gmail.com>
2020-01-28 15:46:45 +0200

    validation get params check

--- src/main/java/com/example/demo/dao/NetworkRepository.java
+++ src/main/java/com/example/demo/dao/NetworkRepository.java
@@ -12,31 +12,39 @@ import java.util.List;
 
 @Repository
 public class NetworkRepository {
-       private final JdbcTemplate jdbcTemplate;
+    private final JdbcTemplate jdbcTemplate;
     private final NodeRepository nodeRepository;
 
     @Autowired
-    public NetworkRepository(JdbcTemplate jdbcTemplate,NodeRepository nodeRepository) {
+    public NetworkRepository(JdbcTemplate jdbcTemplate, NodeRepository nodeRepository) {
         this.jdbcTemplate = jdbcTemplate;
         this.nodeRepository = nodeRepository;
     }
-    public void deleteNetwork(String rootIdentifier){
+
+    public void deleteNetwork(String rootIdentifier) {
         Integer rootId = jdbcTemplate.queryForObject(NodeQuery.GET_ID_BY_IDENTIFIER, Integer.class, rootIdentifier);
         List<Node> networkNodes = jdbcTemplate.query(NodeQuery.SELECT_HIERARCHY_NETWORK_BY_ROOT, new Object[]{rootId}, new NodeMapper());
-        for (Node node:networkNodes) {
+        for (Node node : networkNodes) {
             jdbcTemplate.update(NodeQuery.DELETE_NODE, node.getIdDB());
 
         }
     }
-    public List<Node> searchInRootByName(String name){
-        List<Node>  nodes =
-                jdbcTemplate.query(NodeQuery.SELECT_ROOTS+ NodeQuery.CONDITION_SEARCH_BY_NAME, new Object[]{"%"+name+"%"}, new NodeMapper());
-        for (Node node :nodes) {
-           nodeRepository.getParams(node);
+
+    public List<Node> searchInRootByName(String name) {
+        List<Node> nodes =
+                jdbcTemplate.query(NodeQuery.SELECT_ROOTS + NodeQuery.CONDITION_SEARCH_BY_NAME, new Object[]{"%" + name + "%"}, new NodeMapper());
+        for (Node node : nodes) {
+            nodeRepository.getParams(node);
         }
         return nodes;
     }
-    public List<Node> searchInNodesByName(String name,int rootId){
-        return jdbcTemplate.query(NodeQuery.SELECT_HIERARCHY_NETWORK_BY_ROOT + NodeQuery.CONDITION_SEARCH_BY_NAME_NODE, new Object[]{rootId,"%"+name+"%"}, new NodeMapper());
+
+    public List<Node> searchInNodesByName(String name, int rootId) {
+        List<Node> nodes = jdbcTemplate.query(NodeQuery.SELECT_HIERARCHY_NETWORK_BY_ROOT + NodeQuery.CONDITION_SEARCH_BY_NAME_NODE, new Object[]{rootId, "%" + name + "%"}, new NodeMapper());
+        for (Node node : nodes) {
+            nodeRepository.getParams(node);
+        }
+        return nodes;
+
     }
 }
